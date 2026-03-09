dd19f92f234221c8c022dbd372d8f115f7bbb2f6
Pattaroy <o2481531@student.onesfc.ac.uk>
2026-03-09 14:44:52 +0000

    Initialize synthesizer app structure and UI
    Added initial setup for synthesizer app with note handling and UI elements.

--- NEA_Synthesizer.py
+++ NEA_Synthesizer.py
@@ -5,6 +5,24 @@ from sound import Sound
 from ADSR import ADSR
 from notes import notes
 
+piano{}
+
 app = App(title="Synthesizer")
 
+def current_note(note):
+  current_note = note
 
+
+for note in range 1, notes:
+  if "#" in notes[note]:
+    piano[f"key{note}"] = PushButton(app, 
+    
+
+amplitude1 = slider(app, end = 1, horizontal = false)
+amplitude2 = slider(app, end = 1, horizontal = false)
+
+duration1
+duration2
+
+wave1 = Sound
+wave2
