c4d772b2633578e9a57a4f4e394c7adf44e87560
Pattaroy <o2481531@student.onesfc.ac.uk>
2026-03-09 11:08:11 +0000

    Create synthesizer.py

---
+++ synthesizer.py
@@ -0,0 +1,12 @@
+#synthesizer.py
+
+from sound import sound
+from ADSR import ADSR
+from notes import notes
+import numpy as np
+import sounddevice as sd
+
+class Synthesizer():
+    def __init__(self, sound: np.array, ADSR:np.array):
+        sound
+    
