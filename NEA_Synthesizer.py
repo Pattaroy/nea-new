8b36e81eb85383464e9bc1691d5098937a71005c
Pattaroy <o2481531@student.onesfc.ac.uk>
2026-03-09 12:14:19 +0000

    Update NEA_Synthesizer.py

--- NEA_Synthesizer.py
+++ NEA_Synthesizer.py
@@ -1,6 +1,10 @@
 #NEA_Synthesizer
 
-from guizero import app, Text, PushButton
-from sound import sound
+from guizero import App, Text, PushButton, Slider
+from sound import Sound
 from ADSR import ADSR
 from notes import notes
+
+app = App(title="Synthesizer")
+
+
